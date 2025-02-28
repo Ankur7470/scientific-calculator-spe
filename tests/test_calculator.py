1f10ba88eefec7ef45caa60bb2aab7d86f82a3fe
Ankur Sinha <ankur.sinha2019@gmail.com>
2025-02-28 17:16:57 +0530

    Update test_calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))  # Add parent directory
 import pytest
 from calculator import square_root, factorial, natural_log, power
 
